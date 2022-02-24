4a6b7b590ff21362281a007d1298b3a6113cea2a
Anupam Mondal <a09051985@gmail.com>
2022-02-24 20:51:34 +0530

    240220222051pm

---
+++ wp-content/themes/twoktwoone/index.php
@@ -0,0 +1,13 @@
+<?php get_header(); ?>
+
+<header class="bg-primary">
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <h1 class="text-center text-white my-5 py-5">Hello, world!</h1>
+            </div>
+        </div>
+    </div>
+</header>
+
+<?php get_footer(); ?>
